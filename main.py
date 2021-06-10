5792d3be48bb5623b3dd20bc7cda1b91a6359b12
a.pavlov <a.pavlov@yadro.com>
2021-06-10 17:57:29 +0300

    some changes

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ bot = telebot.TeleBot('1438451238:AAHYpWwXrXwnAMRBCTtQtZdxPJZXwU0MDNg')
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, 'Я бот. Приятно познакомиться')
+    bot.reply_to(message, 'Я бот. Приятно познакомиться. Если хочешь отметиться на паре, то введи команду /buttom')
 
 
 @bot.message_handler(commands=['buttom'])
 def send_welcome(message):
-    bot.reply_to(message, 'Отмечаюсь в классе')
+    bot.reply_to(message, 'Отмечаюсь на паре')
     click_all()
 
 
--- utils/startClass.py
+++ utils/startClass.py
@@ -2,36 +2,47 @@
 
 import sys
 from selenium import webdriver
+
+import json
+import time
+import os
+import base64
 from selenium.webdriver.chrome.options import Options
 
-
-students_credentials = {
-    'login': 'password',
-}
+DB_DIR = os.path.join(os.path.dirname(__file__), '..', 'resources')
 
 
 class Click:
     def __init__(self, name, password):
         self.name = name
-        self.password = password
+        self.password = base64.b64decode(password).decode("utf-8")
 
     def login(self):
-        driver = webdriver.Chrome("/Users/a.pavlov/Downloads/chromedriver")
-        driver.get("https://lk.sut.ru/cabinet/#")
-        users = driver.find_element_by_name('users')
-        parole = driver.find_element_by_name('parole')
-        users.send_keys(self.name)
-        parole.send_keys(self.password)
-        driver.find_element_by_name('logButton').click()
-
-    # def start_class(self):
+        try:
+            driver = webdriver.Chrome("/Users/a.pavlov/Downloads/chromedriver")
+            driver.get("https://lk.sut.ru/cabinet/#")
+            driver.implicitly_wait(5)
+            users = driver.find_element_by_name('users')
+            parole = driver.find_element_by_name('parole')
+            users.send_keys(self.name)
+            parole.send_keys(self.password)
+            driver.find_element_by_name('logButton').click()
+            driver.find_element_by_xpath("//div//font[@title='Учеба...']//..").click()
+            time.sleep(2)
+            driver.find_element_by_xpath("//a[text()='Расписание']").click()
+            time.sleep(2)
+            driver.find_element_by_xpath("//a[text()='Начать занятие']").click()
+        except Exception as exc:
+            print(f'Error: {exc}')
 
 
 def click_all():
-
-    for name, password in students_credentials.items():
-        a = Click(name, password)
-        a.login()
+    for item in os.listdir(DB_DIR):
+        with open(os.path.join(DB_DIR, item), 'r') as f:
+            data = json.loads(f.read())
+            for name, password in data.items():
+                a = Click(name, password)
+                a.login()
 
 
 def __main__():
